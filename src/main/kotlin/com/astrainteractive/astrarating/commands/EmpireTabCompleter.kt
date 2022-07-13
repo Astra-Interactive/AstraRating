b805e58df21464d8824a2c20eeb3d0735721489a
Roman Makeev <makeevrserg@gmail.com>
2022-07-13 09:21:48 +0300

    Fixed tab completer

--- src/main/kotlin/com/astrainteractive/astrarating/commands/EmpireTabCompleter.kt
+++ src/main/kotlin/com/astrainteractive/astrarating/commands/EmpireTabCompleter.kt
@@ -4,19 +4,15 @@ package com.astrainteractive.astrarating.commands
 import CommandManager
 import com.astrainteractive.astralibs.AstraLibs
 import com.astrainteractive.astralibs.registerTabCompleter
-import com.astrainteractive.astralibs.withEntry
 import org.bukkit.Bukkit
 
 /**
  * Tab completer for your plugin which is called when player typing commands
  */
 fun CommandManager.tabCompleter() = AstraLibs.registerTabCompleter("arating") { sender, args ->
-    if (args.size == 1)
-        return@registerTabCompleter listOf("reload", "like", "dislike", "rating")
-    if (args.size == 2 && (args[0].equals("like", ignoreCase = true) || args[1].equals("dislike", ignoreCase = true)))
-        return@registerTabCompleter Bukkit.getOnlinePlayers().mapNotNull { it.name }
-    return@registerTabCompleter listOf<String>()
+    return@registerTabCompleter when {
+        args.size == 1 -> listOf("reload", "like", "dislike", "rating")
+        args.size == 2 && (args[0].equals("like", ignoreCase = true) || args[0].equals("dislike", ignoreCase = true)) -> return@registerTabCompleter Bukkit.getOnlinePlayers().mapNotNull { it.name }
+        else -> listOf<String>()
+    }
 }
-
-
-
