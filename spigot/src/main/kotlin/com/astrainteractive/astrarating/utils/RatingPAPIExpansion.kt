67d671429c706f0cf003ace59f3db4e91cd87a14
makeevrserg <makeevrserg@gmail.com>
2022-11-02 21:44:50 +0300

    refactor: Added KPlaceholderExpansion

--- spigot/src/main/kotlin/com/astrainteractive/astrarating/utils/RatingPAPIExpansion.kt
+++ spigot/src/main/kotlin/com/astrainteractive/astrarating/utils/RatingPAPIExpansion.kt
@@ -7,28 +7,27 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 
-object RatingPAPIExpansion : PlaceholderExpansion() {
+
+object RatingPAPIExpansion : KPlaceholderExpansion(
+    "erating",
+    "RomanMakeev",
+    "1.0.0"
+) {
     private val cachedTotalRating: CachedTotalRating
         get() = CachedTotalRatingModule.value
-    override fun getIdentifier(): String = "erating"
-
-    override fun getAuthor(): String = "RomanMakeev"
-
-    override fun getVersion(): String = "1.0.0"
 
     /**
      * erating_RomaRoman
      * erating_rating
      */
-    override fun onRequest(player: OfflinePlayer?, params: String): String? {
-        val params = PlaceholderAPI.setPlaceholders(player,params.replace("<","%").replace(">","%"))
+    override fun onRequest(player: OfflinePlayer, params: String): String? {
+        val params = PlaceholderAPI.setPlaceholders(player, params.replace("<", "%").replace(">", "%"))
         Bukkit.getOfflinePlayer(params).let {
-            if (it.firstPlayed != 0L)
-                return cachedTotalRating.getPlayerRating(it.name?:"NULL",it.uniqueId).toString()
+            if (it.firstPlayed != 0L) return cachedTotalRating.getPlayerRating(it.name ?: "NULL", it.uniqueId)
+                .toString()
         }
-        player?.let{
-            if (params=="rating")
-                return cachedTotalRating.getPlayerRating(it.name?:"NULL",it.uniqueId).toString()
+        player?.let {
+            if (params == "rating") return cachedTotalRating.getPlayerRating(it.name ?: "NULL", it.uniqueId).toString()
         }
         return ""
     }
