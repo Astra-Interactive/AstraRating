a87fbdd4b99474f1e0fb020fb8263504c3f9ed40
Roman Makeev <makeevrserg@gmail.com>
2023-11-01 21:57:44 +0300

    feat: add sort usecase

--- modules/shared/src/main/kotlin/ru/astrainteractive/astrarating/feature/playerrating/DefaultPlayerRatingsComponent.kt
+++ modules/shared/src/main/kotlin/ru/astrainteractive/astrarating/feature/playerrating/DefaultPlayerRatingsComponent.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.launch
 import ru.astrainteractive.astralibs.async.AsyncComponent
 import ru.astrainteractive.astrarating.api.rating.api.RatingDBApi
 import ru.astrainteractive.astrarating.dto.UserRatingDTO
+import ru.astrainteractive.astrarating.feature.playerrating.domain.SortRatingUseCase
 import ru.astrainteractive.astrarating.model.PlayerModel
 import ru.astrainteractive.astrarating.model.UserRatingsSort
 import ru.astrainteractive.klibs.mikro.core.dispatchers.KotlinDispatchers
@@ -14,7 +15,8 @@ import ru.astrainteractive.klibs.mikro.core.util.next
 class DefaultPlayerRatingsComponent(
     playerModel: PlayerModel,
     private val dbApi: RatingDBApi,
-    private val dispatchers: KotlinDispatchers
+    private val dispatchers: KotlinDispatchers,
+    private val sortRatingUseCase: SortRatingUseCase
 ) : PlayerRatingsComponent, AsyncComponent() {
     override val model = MutableStateFlow(
         PlayerRatingsComponent.Model(playerModel = playerModel)
@@ -22,37 +24,16 @@ class DefaultPlayerRatingsComponent(
 
     override fun onSortClicked() {
         componentScope.launch(dispatchers.IO) {
-            val newSort = model.value.sort.next(UserRatingsSort.values())
-            val previousUserRatings = model.value.userRatings
-            val newUserRatings = when (newSort) {
-                UserRatingsSort.DATE_ASC -> previousUserRatings.sortedBy {
-                    it.time
-                }
-
-                UserRatingsSort.DATE_DESC -> previousUserRatings.sortedByDescending {
-                    it.time
-                }
-
-                UserRatingsSort.PLAYER_ASC -> previousUserRatings.sortedBy {
-                    it.userCreatedReport?.minecraftName
-                }
-
-                UserRatingsSort.PLAYER_DESC -> previousUserRatings.sortedByDescending {
-                    it.userCreatedReport?.minecraftName
-                }
-
-                UserRatingsSort.RATING_ASC -> previousUserRatings.sortedBy {
-                    it.rating
-                }
-
-                UserRatingsSort.RATING_DESC -> previousUserRatings.sortedBy {
-                    it.rating
-                }
-            }
+            val sort = model.value.sort.next(UserRatingsSort.values())
+            val input = SortRatingUseCase.Input(
+                ratings = model.value.userRatings,
+                sort = sort
+            )
+            val result = sortRatingUseCase.invoke(input)
             model.update {
                 it.copy(
-                    sort = newSort,
-                    userRatings = newUserRatings
+                    sort = sort,
+                    userRatings = result.ratings
                 )
             }
         }
