0218fdf95a39508e2144535529d467022c838af1
Roman Makeev <makeevrserg@gmail.com>
2024-12-04 20:10:29 +0300

    fix build

--- modules/core/src/main/kotlin/ru/astrainteractive/astrarating/core/util/StringDescExt.kt
+++ modules/core/src/main/kotlin/ru/astrainteractive/astrarating/core/util/StringDescExt.kt
@@ -4,15 +4,17 @@ import ru.astrainteractive.astralibs.string.StringDesc
 import ru.astrainteractive.astrarating.core.util.StringDescExt.plus
 
 object StringDescExt {
-    operator fun StringDesc.plus(other: String): StringDesc.Raw {
+    operator fun StringDesc.plus(other: String): StringDesc {
         return when (this) {
             is StringDesc.Raw -> StringDesc.Raw(raw.plus(other))
+            is StringDesc.Plain -> StringDesc.Plain(raw.plus(other))
         }
     }
 
-    operator fun StringDesc.plus(other: StringDesc): StringDesc.Raw {
+    operator fun StringDesc.plus(other: StringDesc): StringDesc {
         return when (other) {
             is StringDesc.Raw -> plus(other.raw)
+            is StringDesc.Plain -> plus(other.raw)
         }
     }
 }
