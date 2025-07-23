e5b9984f765fc93c4ead67b6d1f1cd3298827aa3
Roman Makeev <makeevrserg@gmail.com>
2025-07-23 14:31:09 +0300

    fixup literals rating command

--- modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astrarating/command/rating/RatingCommandNode.kt
+++ modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astrarating/command/rating/RatingCommandNode.kt
@@ -9,6 +9,7 @@ import ru.astrainteractive.astralibs.command.api.argumenttype.StringArgumentType
 import ru.astrainteractive.astralibs.command.api.util.command
 import ru.astrainteractive.astralibs.command.api.util.findArgument
 import ru.astrainteractive.astralibs.command.api.util.hints
+import ru.astrainteractive.astralibs.command.api.util.literal
 import ru.astrainteractive.astralibs.command.api.util.runs
 import ru.astrainteractive.astralibs.command.api.util.stringArgument
 import ru.astrainteractive.astralibs.kyori.KyoriComponentSerializer
@@ -23,61 +24,65 @@ internal fun createRatingCommandNode(
     val kyori by kyoriKrate
     return with(kyori) {
         command("arating") {
-            stringArgument("player") {
-                hints(Bukkit.getOnlinePlayers().map(Player::getName))
-                runs { ctx ->
-                    val player = ctx.source.sender as? Player
-                    if (player == null) {
-                        // todo
-                        return@runs
-                    }
-                    val targetPlayer = ctx.findArgument("player", OfflinePlayerArgument)
-                    val targetPlayerUuid = targetPlayer?.uniqueId
-                    val targetPlayerName = targetPlayer?.name
-                    if (targetPlayerUuid == null || targetPlayerName == null) {
-                        return@runs
-                    }
-                    ratingCommandExecutor.execute(
-                        RatingCommand.Result.OpenPlayerRatingGui(
-                            player = player,
-                            selectedPlayerName = targetPlayerName,
-                            selectedPlayerUUID = targetPlayerUuid
-                        )
-                    )
-                }
-            }
-            stringArgument("like_dislike") {
-                hints(listOf("like", "dislike", "+", "-"))
+            literal("player") {
                 stringArgument("player") {
                     hints(Bukkit.getOnlinePlayers().map(Player::getName))
-
                     runs { ctx ->
-                        val executor = ctx.source.sender as? Player
-                        if (executor == null) {
+                        val player = ctx.source.sender as? Player
+                        if (player == null) {
                             // todo
                             return@runs
                         }
-                        val value = when (ctx.findArgument("like_dislike", StringArgumentType)) {
-                            "like", "+" -> 1
-                            "dislike", "-" -> -1
-                            else -> {
-                                // todo
-                                return@runs
-                            }
-                        }
-                        val ratedPlayer = ctx.findArgument("player", OfflinePlayerArgument)
-                        if (ratedPlayer == null) {
-                            // todo
+                        val targetPlayer = ctx.findArgument("player", OfflinePlayerArgument)
+                        val targetPlayerUuid = targetPlayer?.uniqueId
+                        val targetPlayerName = targetPlayer?.name
+                        if (targetPlayerUuid == null || targetPlayerName == null) {
                             return@runs
                         }
                         ratingCommandExecutor.execute(
-                            RatingCommand.Result.ChangeRating(
-                                value = value,
-                                message = TODO(),
-                                executor = executor,
-                                ratedPlayer = ratedPlayer
+                            RatingCommand.Result.OpenPlayerRatingGui(
+                                player = player,
+                                selectedPlayerName = targetPlayerName,
+                                selectedPlayerUUID = targetPlayerUuid
                             )
                         )
+                    }
+                }
+            }
+            literal("rating") {
+                stringArgument("like_dislike") {
+                    hints(listOf("like", "dislike", "+", "-"))
+                    stringArgument("player") {
+                        hints(Bukkit.getOnlinePlayers().map(Player::getName))
+
+                        runs { ctx ->
+                            val executor = ctx.source.sender as? Player
+                            if (executor == null) {
+                                // todo
+                                return@runs
+                            }
+                            val value = when (ctx.findArgument("like_dislike", StringArgumentType)) {
+                                "like", "+" -> 1
+                                "dislike", "-" -> -1
+                                else -> {
+                                    // todo
+                                    return@runs
+                                }
+                            }
+                            val ratedPlayer = ctx.findArgument("player", OfflinePlayerArgument)
+                            if (ratedPlayer == null) {
+                                // todo
+                                return@runs
+                            }
+                            ratingCommandExecutor.execute(
+                                RatingCommand.Result.ChangeRating(
+                                    value = value,
+                                    message = TODO(),
+                                    executor = executor,
+                                    ratedPlayer = ratedPlayer
+                                )
+                            )
+                        }
                     }
                 }
             }
