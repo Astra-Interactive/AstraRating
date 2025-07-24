1072f7620654d7cb1789451672480ccb88d239f8
Roman Makeev <makeevrserg@gmail.com>
2025-07-24 12:27:40 +0300

    add prefix

--- modules/core/api/src/main/kotlin/ru/astrainteractive/astrarating/core/settings/AstraRatingTranslation.kt
+++ modules/core/api/src/main/kotlin/ru/astrainteractive/astrarating/core/settings/AstraRatingTranslation.kt
@@ -5,60 +5,70 @@ package ru.astrainteractive.astrarating.core.settings
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ru.astrainteractive.astralibs.string.StringDesc
+import ru.astrainteractive.astralibs.string.plus
 import ru.astrainteractive.astralibs.string.replace
+import ru.astrainteractive.astralibs.string.toRaw
 
 @Serializable
 class AstraRatingTranslation(
-    @SerialName("database.success")
-    val dbSuccess: StringDesc.Raw = StringDesc.Raw("&#1B76CAУспешно подключено к базе данных"),
-    @SerialName("database.fail")
-    val dbFail: StringDesc.Raw = StringDesc.Raw("&#ca271bНет подключения к базе данных"),
-    @SerialName("database.error")
-    val dbError: StringDesc.Raw = StringDesc.Raw("&#ca271bПроизошла ошибка в базе данных"),
     @SerialName("general.unknown_error")
     val unknownError: StringDesc.Raw = StringDesc.Raw("&#ca271bНеизвестная ошибка"),
     @SerialName("general.prefix")
-    val prefix: StringDesc.Raw = StringDesc.Raw("&#1B76CA[EmpireItems]"),
+    val prefix: StringDesc.Raw = StringDesc.Raw("&#1B76CA[AR] "),
     @SerialName("general.wrong_usage")
-    val wrongUsage: StringDesc.Raw = StringDesc.Raw("&#1B76CAНеверное использование команды"),
+    val wrongUsage: StringDesc.Raw = prefix
+        .plus("&#1B76CAНеверное использование команды")
+        .toRaw(),
     @SerialName("general.reload")
-    val reload: StringDesc.Raw = StringDesc.Raw("&#1B76CAПерезагрузка плагина"),
+    val reload: StringDesc.Raw = prefix
+        .plus("&#1B76CAПерезагрузка плагина")
+        .toRaw(),
     @SerialName("general.reload_complete")
-    val reloadComplete: StringDesc.Raw = StringDesc.Raw("&#1B76CAПерезагрузка успешно завершена"),
+    val reloadComplete: StringDesc.Raw = prefix
+        .plus("&#1B76CAПерезагрузка успешно завершена")
+        .toRaw(),
     @SerialName("general.no_permission")
-    val noPermission: StringDesc.Raw = StringDesc.Raw("&#ca271bУ вас нет прав!"),
+    val noPermission: StringDesc.Raw = prefix
+        .plus("&#ca271bУ вас нет прав!")
+        .toRaw(),
     @SerialName("general.only_player_command")
-    val onlyPlayerCommand: StringDesc.Raw = StringDesc.Raw("&#ca271bКоманда доступна только игрокам!"),
+    val onlyPlayerCommand: StringDesc.Raw = prefix
+        .plus("&#ca271bКоманда доступна только игрокам!")
+        .toRaw(),
     @SerialName("general.player_not_exists")
-    val playerNotExists: StringDesc.Raw = StringDesc.Raw("&#ca271bТакого игрока нет!"),
+    val playerNotExists: StringDesc.Raw = prefix
+        .plus("&#ca271bТакого игрока нет!")
+        .toRaw(),
     @SerialName("general.cant_rate_self")
-    val cantRateSelf: StringDesc.Raw = StringDesc.Raw("&#ca271bВы не можете поставить рейтинг самому себe!"),
+    val cantRateSelf: StringDesc.Raw = prefix
+        .plus("&#ca271bВы не можете поставить рейтинг самому себe!")
+        .toRaw(),
     @SerialName("general.wrong_message_len")
-    val wrongMessageLen: StringDesc.Raw = StringDesc.Raw(
-        "&#ca271bДлина причина рейтинга должна быть в диапазоне [5;30]"
-    ),
+    val wrongMessageLen: StringDesc.Raw = prefix
+        .plus("&#ca271bДлина причина рейтинга должна быть в диапазоне [5;30]")
+        .toRaw(),
     @SerialName("general.liked_user")
-    private val likedUser: StringDesc.Raw = StringDesc.Raw("&#1B76CAВы повысили рейтинг игрока %player%"),
-    @SerialName("general.please_wait")
-    val pleaseWait: StringDesc.Raw = StringDesc.Raw("&#1B76CAПожалуйста), подождите..."),
+    private val likedUser: StringDesc.Raw = prefix
+        .plus("&#1B76CAВы повысили рейтинг игрока %player%")
+        .toRaw(),
     @SerialName("general.disliked_user")
-    private val dislikedUser: StringDesc.Raw = StringDesc.Raw("&#ca271bВы понизили рейтинг игрока %player%"),
+    private val dislikedUser: StringDesc.Raw = prefix
+        .plus("&#ca271bВы понизили рейтинг игрока %player%")
+        .toRaw(),
     @SerialName("general.max_day_voted")
-    val alreadyMaxDayVotes: StringDesc.Raw = StringDesc.Raw(
-        "&#ca271bВы уже проголосовали максимальное количество раз за день"
-    ),
+    val alreadyMaxDayVotes: StringDesc.Raw = prefix
+        .plus("&#ca271bВы уже проголосовали максимальное количество раз за день")
+        .toRaw(),
     @SerialName("general.max_player_voted")
-    val alreadyMaxPlayerVotes: StringDesc.Raw = StringDesc.Raw(
-        "&#ca271bСегодня вы выдали максимальное возможное количество голосов этому игроку"
-    ),
+    val alreadyMaxPlayerVotes: StringDesc.Raw = prefix
+        .plus("&#ca271bСегодня вы выдали максимальное возможное количество голосов этому игроку")
+        .toRaw(),
     @SerialName("general.click_to_delete_report")
     val clickToDeleteReport: StringDesc.Raw = StringDesc.Raw("&#ca271bНажмите ЛКМ чтобы удалить"),
     @SerialName("general.not_enough_on_server")
-    val notEnoughOnServer: StringDesc.Raw = StringDesc.Raw("&#ca271bВы недостаточно долго были на сервере"),
-    @SerialName("general.not_enough_on_discord")
-    val notEnoughOnDiscord: StringDesc.Raw = StringDesc.Raw("&#ca271bВы недостаточно долго были на дискорд-сервере"),
-    @SerialName("general.need_discord_link")
-    val needDiscordLinked: StringDesc.Raw = StringDesc.Raw("&#ca271bВы должны привязать дискорд /discord link"),
+    val notEnoughOnServer: StringDesc.Raw = prefix
+        .plus("&#ca271bВы недостаточно долго были на сервере")
+        .toRaw(),
     @SerialName("general.player_name_color")
     val playerNameColor: StringDesc.Raw = StringDesc.Raw("&#DDB92B"),
     @SerialName("general.positive_color")
@@ -77,8 +87,6 @@ class AstraRatingTranslation(
     val menuPrevPage: StringDesc.Raw = StringDesc.Raw("&#1B76CAПред. страницы"),
     @SerialName("menu.next_page")
     val menuNextPage: StringDesc.Raw = StringDesc.Raw("&#1B76CAСлед. страница"),
-    @SerialName("menu.back")
-    val menuBack: StringDesc.Raw = StringDesc.Raw("&#1B76CAНазад"),
     @SerialName("menu.close")
     val menuClose: StringDesc.Raw = StringDesc.Raw("&#1B76CAЗакрыть"),
     @SerialName("menu.sort.PLAYER_ASC")
@@ -116,9 +124,9 @@ class AstraRatingTranslation(
     @SerialName("events.kill_player")
     private val killedPlayer: StringDesc.Raw = StringDesc.Raw("&#9c0303Убил игрока %killed_player%"),
     @SerialName("events.you_killed_player")
-    private val youKilledPlayer: StringDesc.Raw = StringDesc.Raw(
-        "&#9c0303Вы убили игрока %killed_player%, ваш рейтинг был понижен"
-    )
+    private val youKilledPlayer: StringDesc.Raw = prefix
+        .plus("&#9c0303Вы убили игрока %killed_player%, ваш рейтинг был понижен")
+        .toRaw()
 ) {
     fun killedPlayer(playerName: String) = killedPlayer.replace("%killed_player%", playerName)
     fun youKilledPlayer(playerName: String) = youKilledPlayer.replace("%killed_player%", playerName)
